9bcd16715a2be3ce42993b643bb42f54172a8f59
Fausto C <faustocamp545@gmail.com>
2023-12-16 22:33:48 -0500

    db package

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,1 +1,5 @@
+from app.db.connection import Base, SessionLocal, engine
 from app.db.repository import PostRepository
+from app.db.repository import UserRepository
+
+__all__ = ["Base", "SessionLocal", "engine", "PostRepository", "UserRepository"]
