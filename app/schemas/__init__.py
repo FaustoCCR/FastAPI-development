06b88cb58f1197089a5954e4167f2e55b988c33d
Fausto C <faustocamp545@gmail.com>
2024-01-28 17:41:15 -0500

    Type login response model route

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -5,5 +5,14 @@ These pydantic models define more or less a "schema" (a valid data shape)
 
 from app.schemas.post import Post, PostCreate
 from app.schemas.user import User, UserCreate, UserLogin
+from app.schemas.token import TokenData, Token
 
-__all__ = ["PostCreate", "Post", "User", "UserCreate", "UserLogin"]
+__all__ = [
+    "PostCreate",
+    "Post",
+    "User",
+    "UserCreate",
+    "UserLogin",
+    "Token",
+    "TokenData",
+]
